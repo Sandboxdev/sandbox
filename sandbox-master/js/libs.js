2e791bfbf93525639977182e214528410b42b161
user1622 <pastushuk.denis@gamil.com//Testing>
2016-07-24 10:02:44 +0300

    new blocks for parallax

---
+++ sandbox-master/js/libs.js
@@ -0,0 +1,45 @@
+$(document).ready(function(){
+
+	$('#mainSection').fullpage({
+		menu:'#responsive-menu',
+		slidesNavigation: true,
+		anchors:['firstSection','secondSection','3rdSection','4rdSection','5rdSection'],
+		afterLoad:function(link, index) { 
+
+			if(link == "secondSection"){
+					//вторая секция, левый блок теста.
+					$('#section2 p,.textSection2 h3').show(function(){
+						$(this).css({opacity: 0.0, visibility: "visible"}).animate({opacity: 1.0})
+					});
+					$('#section2 p,.textSection2 h3').addClass('fadeInLeft');
+
+					//
+					$('.weCanSection2 h3,.weCanSection2 ul').show(function(){
+						$(this).css({opacity: 0.0, visibility: "visible"}).animate({opacity: 1.0})
+					});
+					$('.weCanSection2 h3,.weCanSection2 ul').addClass('fadeInRight');
+					
+					
+				} 
+
+				if(link == "3rdSection") {
+
+				} 
+			},
+			afterRender: function(){
+
+
+					//playing the video
+					$('video').get(0).play();
+				}
+
+		});
+
+	$('#togler-button').click(function(){
+		$('#togler-button-bg').toggleClass('off');
+		$('#rus').toggleClass('rus_off');
+		$('#eng').toggleClass('eng_on');
+
+	});
+	
+});
